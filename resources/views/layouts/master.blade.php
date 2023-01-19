1d902280e3ea9f34ade958a8151f084401b1b8b2
Zeinabou Nani <zeinabounani01@gmail.com>
2023-01-19 20:33:06 +0000

    initial

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,7 +22,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <span class="brand-text font-weight-bold" style="font-size: 1.3em;"><b>YBLTD</b></span>
+      <span class="brand-text font-weight-bold" style="font-size: 1.3em;"><b>neina</b></span>
     </a>
 
     <!-- Sidebar -->
